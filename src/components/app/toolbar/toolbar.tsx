405d9ae9316fab7e90709f4cdf5bb9180795c84a
ardevelop <mail@ardevelop.com>
2016-07-13 17:22:25 +0300

    Add new and adjust current shapes

--- src/components/app/toolbar/toolbar.tsx
+++ src/components/app/toolbar/toolbar.tsx
@@ -9,9 +9,11 @@ export class ToolbarComponent extends React.Component<{}, {}> {
         <ButtonComponent shape="square" />
         <ButtonComponent shape="circle" />
         <ButtonComponent shape="triangle" />
+        <ButtonComponent shape="mixbook" />
         <ButtonComponent shape="homer-simpson" />
         <ButtonComponent shape="sonic" />
         <ButtonComponent shape="slack" />
+        <ButtonComponent shape="react" />
       </div>
     )
   }
